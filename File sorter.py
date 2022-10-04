d0a833ab18b8a15c1333e92fe4e3bfd807073b3e
Saumy <81005589+Saumy-coder@users.noreply.github.com>
2022-10-04 17:04:30 +0530

    Update File sorter.py

--- File sorter.py
+++ File sorter.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 
-print("There should not be any folder withing the initial folder ")
+print("Added a functionality of sub folders \n Note: There should not be any folders withing the subfolder ")
 sourced = input("Enter main folder:")    
 source = sourced.replace("\\",'/')
 destd = input("Enter the folder to create the new folders: ")
@@ -19,10 +19,28 @@ for x in files:
     for y in range(0,len(exten)):
         if (ext == exten[y]):
             source1 = source+'/'+name + ext
-            dest1 = dest +'/'+ext +'/'+name + ext
-            if(os.path.exists(dest+'/'+ext)==True):
+            dest1 = dest +'/'+ext[1:] +'/'+name + ext
+            if(os.path.exists(dest+'/'+ext[1:])==True):
                 paste = shutil.copy(source1,dest1)
             else:
-                os.makedirs(dest+'/'+ext)
+                os.makedirs(dest+'/'+ext[1:])
                 paste = shutil.copy(source1,dest1)
-        
+        elif(ext == ''):
+            subexten = []
+            subpath = source+'/'+ name
+            subfiles = os.listdir(subpath)
+            for p in subfiles:
+                subname,subext = os.path.splitext(p)
+                for a in range(0,len(subfiles)):
+                    if(subext not in subexten):
+                        subexten.append(subext)
+            
+                for b in range(0,len(subexten)):
+                    if(subext==subexten[b]):
+                        subsource1 = subpath + '/' + subname + subext
+                        subdest1 = dest +'/'+ subext[1:] +'/'+name + '/' +subname + subext
+                        if(os.path.exists(dest +'/'+ subext[1:] +'/'+name)==True):
+                            paste = shutil.copy(subsource1,subdest1)
+                        else:
+                            os.makedirs(dest +'/'+ subext[1:] +'/'+name)
+                            paste = shutil.copy(subsource1,subdest1)
