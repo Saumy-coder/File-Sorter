99518ae9bcc4530a69fc3ad9eb9ff601e743f09c
Saumy <81005589+Saumy-coder@users.noreply.github.com>
2022-10-04 21:43:41 +0530

    Update File sorter.py

--- File sorter.py
+++ File sorter.py
@@ -1,12 +1,14 @@
 import os
 import shutil
 
-
-print("Added a functionality of sub folders \n Note: There should not be any folders withing the subfolder ")
+print("Added a functionality of sub folders \nNote: There should not be any folders withing the subfolder ")
 sourced = input("Enter main folder:")    
 source = sourced.replace("\\",'/')
 destd = input("Enter the folder to create the new folders: ")
 dest = destd.replace("\\",'/')
+
+perform = int(input("1. For moving \n 2. For copying: \n"))
+
 
 files = os.listdir(source)
 exten = []
@@ -21,10 +23,20 @@ for x in files:
             source1 = source+'/'+name + ext
             dest1 = dest +'/'+ext[1:] +'/'+name + ext
             if(os.path.exists(dest+'/'+ext[1:])==True):
-                paste = shutil.copy(source1,dest1)
+                if(perform==2):
+                    paste = shutil.copy(source1,dest1)
+                elif(perform==1):
+                    paste = shutil.move(source1,dest1)
+                else:
+                    print("Please choose between 1 or 2")
             else:
                 os.makedirs(dest+'/'+ext[1:])
-                paste = shutil.copy(source1,dest1)
+                if(perform==2):
+                    paste = shutil.copy(source1,dest1)
+                elif(perform==1):
+                    paste = shutil.move(source1,dest1)
+                else:
+                    print("Please put between 1 or 2")
         elif(ext == ''):
             subexten = []
             subpath = source+'/'+ name
@@ -40,7 +52,13 @@ for x in files:
                         subsource1 = subpath + '/' + subname + subext
                         subdest1 = dest +'/'+ subext[1:] +'/'+name + '/' +subname + subext
                         if(os.path.exists(dest +'/'+ subext[1:] +'/'+name)==True):
-                            paste = shutil.copy(subsource1,subdest1)
+                            if(perform==1):
+                                paste = shutil.move(subsource1,subdest1)
+                            elif(perform==2):
+                                paste = shutil.copy(subsource1,subdest1)
                         else:
                             os.makedirs(dest +'/'+ subext[1:] +'/'+name)
-                            paste = shutil.copy(subsource1,subdest1)
+                            if(perform==1):
+                                paste = shutil.move(subsource1,subdest1)
+                            elif(perform==2):
+                                paste = shutil.copy(subsource1,subdest1)
